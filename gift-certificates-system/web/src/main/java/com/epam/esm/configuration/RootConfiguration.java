a1e52843b468f381e580122e07ace7ce1dba9cfa
Alexandra Podgayskaya <Sasha241992Pod@gmail.com>
2021-06-11 14:02:56 +0300

    Update configuration

--- gift-certificates-system/web/src/main/java/com/epam/esm/configuration/RootConfiguration.java
+++ gift-certificates-system/web/src/main/java/com/epam/esm/configuration/RootConfiguration.java
@@ -3,20 +3,18 @@ package com.epam.esm.configuration;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
-
 @PropertySource("classpath:db.properties")
-@ComponentScan("com.epam.esm")
-public class PersistenceConfiguration {
-
+public class RootConfiguration {
 	@Value("${db.driver}")
 	private String driverClassName;
 	@Value("${db.url}")
@@ -28,8 +26,8 @@ public class PersistenceConfiguration {
 	@Value("${db.poolsize}")
 	private int poolSize;
 
-	//TODO profile, add "dev"
-	@Profile ("prod")
+	// TODO profile, add "dev"
+	@Profile("prod")
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
@@ -44,6 +42,15 @@ public class PersistenceConfiguration {
 	@Bean
 	public JdbcTemplate jdbcTemplate() {
 		return new JdbcTemplate(dataSource());
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper mapper = new ModelMapper();
+		// TODO разобраться
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT).setFieldMatchingEnabled(true)
+				.setSkipNullEnabled(true).setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+		return mapper;
 	}
 
 }
